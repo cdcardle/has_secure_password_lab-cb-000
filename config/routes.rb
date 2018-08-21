ab6ec365a2ab8521446ffe4dc54bd9a29804ad28
Chris Cardle <cdcardle@gmail.com>
2018-08-21 14:00:54 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/users/:id/homepage', to: 'users#homepage'
 
+  root '/login', to: 'users#new'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
