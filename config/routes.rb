2ebe0e6d0581c2c890bebe88ed17bbbd95d4e401
Chris Cardle <cdcardle@gmail.com>
2018-08-21 14:26:28 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
 
   get "sessions/create"
 
-  get "users/homepage"
-
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
