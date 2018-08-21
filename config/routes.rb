7c96dcd2aae3f89abde35b72c46389c0f8fcf863
Chris Cardle <cdcardle@gmail.com>
2018-08-21 13:57:57 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'user#homepage'
+  root 'users#homepage'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
