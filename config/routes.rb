fa49cbb9df798b71aedf5dea134ea3a99daea31b
Chris Cardle <cdcardle@gmail.com>
2018-08-21 14:24:07 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  get '/welcome/homepage', to: 'welcome#homepage'
+  get "/welcome/homepage", to: "welcome#homepage"
 
   get 'sessions/create'
 
